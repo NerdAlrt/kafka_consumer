3bac3918c369a9ebb75dc144f843550a7faa53ed
NerdAlrt <diwakar.thakur1994@gmail.com>
2025-03-26 15:46:29 +0530

    modified controller

--- src/main/java/com/example/consumer/controller/GreetController.java
+++ src/main/java/com/example/consumer/controller/GreetController.java
@@ -4,13 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-@RestController("/api")
+@RestController
 public class GreetController {
 	
 	@GetMapping("/greet")
 	public ResponseEntity<String> greetUser(){
 		
 		return new ResponseEntity<String>("Hello there!", HttpStatus.OK);
+	}
+	
+	@GetMapping("hello")
+	public String sayHello() {
+		return "Hello!";
 	}
 
 }
